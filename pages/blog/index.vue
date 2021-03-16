dba8d3eb396e8100f5bf2ead118ea7fbb2e0afc9
“Adrian” <desarrolloweb.cya@gmail.com>
2021-03-16 17:07:53 -0600

    armando canasta

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,7 +2,7 @@
     <div class="ps-page--blog">
         <div class="container">
             <div class="ps-page__header">
-                <h1>Our Press</h1>
+                <h1>Our Press elimine archivso</h1>
                 <bread-crumb2 :breadcrumb="breadCrumb" />
             </div>
             <blog-grid />
