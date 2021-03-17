72e6d563e0d25ed686a68227a80519f689956cb6
“Adrian” <desarrolloweb.cya@gmail.com>
2021-03-17 14:40:07 -0600

    botones

--- components/partials/homepage/default/ProductFilter.vue
+++ components/partials/homepage/default/ProductFilter.vue
@@ -1,8 +1,72 @@
-<template>
-    <div class="ps-filter ps-product-filter">
+<template lang="html">
+    <div class="ps-filter ">
         <div class="container">
-            <div class="ps__content">
-                
+            <div class="ps-block-content--armar">
+                <div  class="ps-filter--armar">
+                    <a>
+                        Vino
+                    </a>
+                </div>
+
+                <div  class="ps-filter--armar">
+                    <a>
+                        Wisky
+                    </a>
+                </div>
+
+                <div  class="ps-filter--armar">
+                    <a>
+                        Galletas
+                    </a>
+                </div>
+
+                <div  class="ps-filter--armar">
+                    <a>
+                        Domino
+                    </a>
+                </div>
+
+                <div  class="ps-filter--armar">
+                    <a>
+                        Chocolates
+                    </a>
+                </div>
+
+                <div  class="ps-filter--armar">
+                    <a>
+                        Arcon
+                    </a>
+                </div>
+
+                <div  class="ps-filter--armar">
+                    <a>
+                        Canasta
+                    </a>
+                </div>
+
+                <div  class="ps-filter--armar">
+                    <a>
+                        fdghbjnm
+                    </a>
+                </div>
+                <div  class="ps-filter--armar">
+                    <a href="/shop"
+                    
+                    >
+                        dsxfgbc
+                    </a>
+                </div>
+
+                <div  class="ps-filter--armar">
+                    <a
+                        href="/shop"
+                        @click="filter"
+
+                    >
+                        dxfgbc
+                    </a>
+                </div>
+
             </div>
         </div>
     </div>
@@ -10,6 +74,22 @@
 
 <script>
 export default {
-    name:'PrdocutFilter'
+    name:'PrdocutFilter',
+
+
+    methods:{
+        filter(){
+           if (slug) {
+                const url = `/shop?category=${slug}`;
+                const products = getColletionBySlug(this.categories, slug);
+                this.$store.commit('product/setProducts', products);
+                this.$store.commit('product/setProducts', products);
+                this.$store.commit('product/setTotal', products.length);
+                this.$store.commit('collection/setQueries', [slug]);
+                this.$router.push(url);
+            }
+        },
+    },
+
 }
 </script>
